e89731868f0e0d633b22c9be4bfb67d04188ce51
qiufeng1994 <631368830@qq.com>
2019-05-13 16:50:45 +0800

    hand segmentation

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,4 +1,6 @@
+import os
 import importlib
+import warnings
 
 
 class WriterTensorboardX():
@@ -9,9 +11,9 @@ class WriterTensorboardX():
             try:
                 self.writer = importlib.import_module('tensorboardX').SummaryWriter(log_path)
             except ModuleNotFoundError:
-                message = "Warning: TensorboardX visualization is configured to use, but currently not installed on this machine. " + \
-                          "Please install the package by 'pip install tensorboardx' command or turn off the option in the 'config.json' file."
-                logger.warning(message)
+                message = """TensorboardX visualization is configured to use, but currently not installed on this machine. Please install the package by 'pip install tensorboardx' command or turn off the option in the 'config.json' file."""
+                warnings.warn(message, UserWarning)
+                logger.warn()
         self.step = 0
         self.mode = ''
 
